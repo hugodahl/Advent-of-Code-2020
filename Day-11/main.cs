c2c507714f9755838f351e123802d0467c15faa3
Hugo Dahl <hugo@code-jedi.com>
2020-12-11 07:35:26 -0600

    Add basics for Day 11

---
+++ Day-11/main.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq
+
+
+namespace HugoDahl.AdventOfCode.Year2020
+{
+
+	public class Program
+	{
+
+		public static int Main(string[] args
+		{
+			return 0;
+		}
+
+	}
+
+}
