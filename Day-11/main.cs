0e3d54419f3666f2701fcdc621c4cf81cca97dd5
Hugo Dahl <hugo@code-jedi.com>
2020-12-12 00:02:30 -0600

    Include WIP for Day 11

--- Day-11/main.cs
+++ Day-11/main.cs
@@ -26,7 +26,7 @@ namespace HugoDahl.AdventOfCode.Year2020
 
 		public static int Main(string[] args)
 		{
-			var inputs = GetInput(false);
+			var inputs = GetInput(true);
 
       var listed = inputs.ToList();
 
@@ -37,22 +37,61 @@ namespace HugoDahl.AdventOfCode.Year2020
                  .ToList();
 
       var size = GetFileContentInfo(listed);
+      var maxRounds = 10;
 
-var round = 1;
-      parsed.ForEach(row => {
-        row.ForEach(cell => {
-          var contigs = cell.GetNeighbours(size.width, size.height)
-                            .Select(n => parsed[n.X][n.Y]);
-          cell.CalculateBusy(round, contigs);
+//       for(var round = 1; round <= maxRounds; round++){
+//         parsed.ForEach(row => {
+//           row.ForEach(cell => {
+//             var contigs = cell.GetNeighbours(size.width, size.height)
+//                               .Select(n => parsed[n.X][n.Y]);
+//             cell.CalculateBusy(round, contigs);
+//           });
+//         });
+//       }
+
+
+
+      for(var round = 1; round <= maxRounds; round++){
+        parsed.ForEach(row => {
+          row.ForEach(cell => {
+            cell.CalculateVisibleBusy(round, size.width, size.height, parsed);
+          });
         });
-      });
+      }
 
-      var first = parsed.First().First();
-      
-      Console.WriteLine($"{first} => {first.IsOccupied(1)}");
+
+      var result = Enumerable.Range(1, maxRounds)
+                             .Select(round => {
+                                        var row = parsed[0].Count(c => c.IsOccupied(round));
+                                        
+                                        var rowResult = new {
+                                          Round = round, 
+                                          Occupieds = parsed.Sum(row => row.Count(cell => cell.IsOccupied(round))),
+                                        };
+
+                                        return rowResult;
+                                    }
+      );
+
+      result.Select(res => $"Round: {res.Round};  {res.Occupieds} occupied seats")
+            .ToList()
+            .ForEach(Console.WriteLine);
 
       return 0;
 		}
+
+    private static void PrintGrid(IEnumerable<IEnumerable<FloorLocation>> parsed, int maxRounds){
+      foreach(var round in Enumerable.Range(1, maxRounds))
+      {
+          var grid = string.Join(Environment.NewLine, parsed.Select(row => string.Join("", row.Select(cell => cell.GetState(round)))));
+
+          Console.WriteLine($"----------------");
+          Console.WriteLine($"---Round #{round}---");
+          Console.WriteLine($"-------------");
+          Console.WriteLine(grid);
+          Console.WriteLine($"----------------");
+       }
+    }
 
     private static void PopulateInputArray<T>(IEnumerable<T> inputs, char[,] array){
       var indexed = inputs.Select((value, idx) => new {Value = value, Index = idx}).ToList();
@@ -106,6 +145,10 @@ var round = 1;
       return letter.Equals('#');
     }
 
+    public static bool IsBetween(this int value, int min, int max){
+      return value >= min && value <= max;
+    }
+
   }
 
 
@@ -129,14 +172,54 @@ var round = 1;
       return this._rounds[round];
     }
 
+    public void CalculateVisibleBusy(int round, int maxRows, int maxColumns, List<List<FloorLocation>> seats){
+      if (this.IsFloor){
+        return;
+      }
+
+      var prevState = this.IsOccupied(round -1);
+
+      var visibles = this.GetVisibleSeats(maxRows, maxColumns, seats)
+                         .Select(pos => seats[pos.X][pos.Y])
+                         .ToList();
+
+      var busy = visibles.Where(seat => seat.IsOccupied(round-1))
+                         .Count();
+
+      if (!prevState){ // If we're empty
+        this._rounds.Add(busy == 0);
+        return;
+      }
+
+      if (busy > 5){
+        this._rounds.Add(false);
+        return;
+      }
+
+      this._rounds.Add(prevState);
+    }
+
     public void CalculateBusy(int round, IEnumerable<FloorLocation> neighbours){
       if (this.IsFloor){
         return;
       }
 
-      var busy = neighbours.Where(n => n.IsSeat).Where(seat => seat.IsOccupied(round-1)).Count();
-      var occupied = busy <= 4;
-      this._rounds.Add(occupied);
+      var prevState = this.IsOccupied(round -1);
+
+      var busy = neighbours.Where(seat => seat.IsOccupied(round-1))
+                           .Count();
+
+      if (!prevState){ // If we're empty
+        this._rounds.Add(busy == 0);
+        return;
+      }
+
+      if (busy > 4){
+        this._rounds.Add(false);
+        return;
+      }
+
+      this._rounds.Add(prevState);
     }
 
     public static FloorLocation Parse(char value, Point position){
@@ -152,9 +235,9 @@ var round = 1;
     public IEnumerable<Point> GetNeighbours(int maxRows, int maxCols){
 
       var minCol = Math.Max(0, this.Position.X -1);
-      var maxCol = Math.Min(maxCols, this.Position.X + 1);
+      var maxCol = Math.Min(maxCols-1, this.Position.X + 1);
       var minRow = Math.Max(0, this.Position.Y - 1);
-      var maxRow = Math.Min(maxRows, this.Position.Y + 1);
+      var maxRow = Math.Min(maxRows-1, this.Position.Y + 1);
 
       var points = new List<Point>();
 
@@ -165,17 +248,51 @@ var round = 1;
       }
     }
 
+    public IEnumerable<Point> GetVisibleSeats(int maxRows, int maxCols, List<List<FloorLocation>> grid){
+      var increments = new [] {-1, 0, 1};
+
+      foreach(var x in increments){
+        foreach(var y in increments){
+          if (x == 0 && y == 0){
+            continue; // Don't look at ourselves
+          }
+
+          var currX = this.Position.X + x;
+          var currY = this.Position.Y + y;
+
+          while (currX.IsBetween(0, maxRows -1) && currY.IsBetween(0, maxCols -1)){
+
+            if (currX == this.Position.X && currY == this.Position.Y){
+              continue; // Avoid looking at ourselves, again.
+            }
+
+            if (grid[currX][currY].IsSeat){
+              yield return new Point(currX, currY);
+              break;
+            }
+
+            currX += x;
+            currY += y;
+
+          }
+
+        }
+      }
+
+    }
+
     public override string ToString(){
-      return $"{IsSeat ? 'L' : '.'} @ {Position}";
+      return $"{(IsSeat ? 'L' : '.')} @ {Position}";
     }
 
     public char GetState(int round){
-      if (round > this._rounds.Count){
-        return 'X';
-      }
 
       if (this.IsFloor){
         return '.';
+      }
+
+      if (round > this._rounds.Count){
+        return 'X';
       }
 
       return this._rounds[round] ? '#' : 'L';
